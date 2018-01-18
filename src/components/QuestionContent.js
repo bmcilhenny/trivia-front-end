04bfa56df33ff195e4755eb8d2b64f55abdf5831
Josh-Stillman <joshstillman@gmail.com>
2018-01-18 16:37:54 -0500

    got buzzer timer working

--- src/components/QuestionContent.js
+++ src/components/QuestionContent.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 
-const QuestionContent = (props) => {
+class QuestionContent extends React.Component {
 
-  const shuffle = (a) => {
+  constructor(){
+    super()
+
+    this.state ={
+      count: 10,
+      shuffled: []
+    }
+  }
+
+
+  shuffle = (a) => {
       for (let i = a.length - 1; i > 0; i--) {
           const j = Math.floor(Math.random() * (i + 1));
           [a[i], a[j]] = [a[j], a[i]];
@@ -11,16 +21,50 @@ const QuestionContent = (props) => {
       return a;
   }
 
-  const allAnswersArray = [props.correctAnswer, ...props.incorrectAnswers];
-  const shuffled = shuffle(allAnswersArray);
+  startBuzzer = () => {
+    window.addEventListener('keydown', this.handleBuzz)
+
+    window.setInterval(() => this.setState({count: this.state.count -1}), 1000)
+  }
+
+  handleBuzz = (event) => {
+    console.log(event)
+    if (event.code === "ShiftLeft") {
+        alert("Player 1")
+    } else if (event.code === "ShiftRight") {
+        alert("Player 2")
+    }
+  }
+
+
+  componentWillMount(){
+    const allAnswersArray = [this.props.correctAnswer, ...this.props.incorrectAnswers]
+    const shuffledArray = this.shuffle(allAnswersArray)
+    this.setState({shuffled: shuffledArray}, this.startBuzzer());
+
+  }
+  //
+  // componentWillUnmount(){
+  //
+  // }
+  //
+  // getBuzz = () => {
+  //   setTimeOut(, 10000)
+  // }
+
+
+  render(){
+
 
   return (
     <div>
       <h1>Question Time!</h1>
-      <h3>{props.question}</h3>
-      {shuffled.map(answer => <button key={answer.id}>{answer}</button>)}
+      <h2>{this.state.count} second left</h2>
+      <h3>{this.props.question}</h3>
+      {this.state.shuffled.map(answer => <button onClick={this.props.guess} key={answer.id} name={answer === this.props.correctAnswer ? "correct" : "incorrect"} >{answer}</button>)}
     </div>
   )
+}
 }
 
 export default QuestionContent;
--- src/containers/QuestionContainer.js
+++ src/containers/QuestionContainer.js
@@ -18,17 +18,34 @@ class QuestionContainer extends React.Component {
 
   removeWeirdEncoding = (json) => {
     json.forEach(question => {
-      question.question = question.question.replace('&quot;', '"').replace('&#039;', "'");
-      question.correct_answer = question.correct_answer.replace('&quot;', '"').replace('&#039;', "'");
-      question.incorrect_answers[0] = question.incorrect_answers[0].replace('&quot;', '"').replace('&#039;', "'");
-      question.incorrect_answers[1] = question.incorrect_answers[1].replace('&quot;', '"').replace('&#039;', "'");
-      question.incorrect_answers[2] = question.incorrect_answers[2].replace('&quot;', '"').replace('&#039;', "'");
+      question.question = question.question.replace(/&quot;/g, '"').replace(/&#039;/g, "'");
+      question.correct_answer = question.correct_answer.replace(/&quot;/g, '"').replace(/&#039;/g, "'");
+      question.incorrect_answers[0] = question.incorrect_answers[0].replace(/&quot;/g, '"').replace(/&#039;/g, "'");
+      question.incorrect_answers[1] = question.incorrect_answers[1].replace(/&quot;/g, '"').replace(/&#039;/g, "'");
+      question.incorrect_answers[2] = question.incorrect_answers[2].replace(/&quot;/g, '"').replace(/&#039;/g, "'");
     })
     return json;
   }
 
   getQuestions = () => {
-    let myBody = {difficulty: 'easy', category: '22'};
+
+    let currentDifficulty
+    switch (this.state.currentRound) {
+      case 1:
+        currentDifficulty = "easy"
+        break;
+      case 2:
+        currentDifficulty = "medium"
+        break;
+      case 3:
+        currentDifficulty = "hard"
+        break;
+    }
+
+    let randomCategory = Math.floor(Math.random() * (32 - 9 + 1)) + 9
+    console.log("calling API with", randomCategory, currentDifficulty)
+
+    let myBody = {difficulty: currentDifficulty, category: randomCategory};
     fetch('http://localhost:3000/api/v1/get_questions', {
       method: "POST",
       headers: {
@@ -37,19 +54,61 @@ class QuestionContainer extends React.Component {
       },
       body: JSON.stringify(myBody)
     }).then(resp => resp.json())
-    .then(json => json.results)
+    .then(json => {if (json.response_code === 1) {
+        this.getQuestions();
+    } console.log(json); return json.results})
     .then(results => this.removeWeirdEncoding(results))
     .then(cleanedResults => this.setState({
-      questions: cleanedResults
+      questions: cleanedResults, currentQuestion: 0
     }))
   }
 
+  guess = (event) => {
+    if (event.target.name === "correct") {
+      alert("right!")
+      //points, etc.
+      this.nextQuestion()
+    }else {
+      alert("wrong!")
+      //slops
+    }
+
+  }
+
+  nextQuestion = () => {
+    //are we at the end?  if so next round
+    //otherwise load up next Q and re-render
+    if (this.state.currentQuestion === 9) {
+      this.nextRound()
+    } else{
+      this.setState({currentQuestion: this.state.currentQuestion + 1})
+    }
+  }
+
+  nextRound = () => {
+    if (this.state.currentRound === 3) {
+      alert("game over!")
+      //call game over function
+    } else {
+      this.setState({currentRound: this.state.currentRound + 1}, () => this.getQuestions())
+    }
+
+  }
+
+
+
   render () {
     console.log(this.state.questions)
+
+
+
+
+
     const currentQuestion = this.state.currentQuestion;
     return (
       <div>
-        {this.state.questions.length ? <QuestionContent question={this.state.questions[currentQuestion].question} correctAnswer={this.state.questions[currentQuestion].correct_answer} incorrectAnswers={this.state.questions[currentQuestion].incorrect_answers}/> : 'Nothing'}
+        <h3>Current Round {this.state.currentRound}</h3>
+        {this.state.questions.length ? <QuestionContent question={this.state.questions[currentQuestion].question} correctAnswer={this.state.questions[currentQuestion].correct_answer} incorrectAnswers={this.state.questions[currentQuestion].incorrect_answers} guess={this.guess}/> : 'Loading'}
       </div>
     )
   }

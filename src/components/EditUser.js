2bbcfd9e7fc4569885b0afbd904d81d0224da7a5
bmcilhenny <bmcilhen@gmail.com>
2018-01-24 14:17:53 -0500

    formatting complete for new and edit user pages

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,5 +1,6 @@
-import React from 'react'
-
+import React from 'react';
+import {Menu, Container, Button, Header, Icon, Dropdown, Segment, Form} from 'semantic-ui-react';
+import { BrowserRouter as Router, Route, Redirect, Link } from 'react-router-dom';
 
 class EditUser extends React.Component {
 
@@ -34,10 +35,12 @@ class EditUser extends React.Component {
     this.setState({[newVal]: event.target.value})
   }
 
-  handleSelect = event => {
-    let newVal = event.target.id;
-    let user = this.state.players.find(player => player.id === parseInt(event.target.value))
+  handleSelect = (event, data) => {
+    console.log(event.target)
+    let newVal = data.id;
+    let user = this.state.players.find(player => player.id === parseInt(data.value))
     console.log(user)
+
     this.setState({
       [newVal]: event.target.value,
       name: user.name,
@@ -72,25 +75,79 @@ class EditUser extends React.Component {
     }, () => this.props.routerProps.history.push("/startgame"))
   }
 
+  createOptionsForForm = () => {
+    let newArr = []
+    this.state.players.forEach(el => newArr.push(Object.assign({text: el.name, key: el.id, value: el.id})))
+    return newArr;
+  }
+
 
   render() {
-    console.log(this.props)
+    console.log(this.state)
     return (
       <div>
-        <h1>Edit a User</h1>
-        <form onSubmit={this.editUser}>
-          <select value={this.state.selectedUser} onChange={this.handleSelect} id="selectedUser">
-            {this.state.players.map(player => <option value={player.id} key={player.id}>{player.name}</option>)}
-          </select>
-          <input type="text" id="name" value={this.state.name} onChange={this.handleChange}/>
-          <input type="text" id="bio" value={this.state.bio} onChange={this.handleChange}/>
-          <input type="text" id="image" value={this.state.image} onChange={this.handleChange}/>
-          <button type="submit" >Confirm</button>
-        </form>
-        <button onClick={this.deleteUser}>Delete</button>
+        <Container>
+          <Menu  inverted pointing secondary size='large'>
+            <Menu.Item as={Link} to="startgame" active>Home</Menu.Item>
+            <Menu.Item as='a'>Careers</Menu.Item>
+            <Menu.Item as='a'>Add a Question</Menu.Item>
+            <Menu.Item as='a'>Api</Menu.Item>
+            <Menu.Item position='right'>
+              <Button as={Link} to="new" inverted className={"white-link"}>Add User</Button>
+              <Button as={Link} to="edit" inverted style={{ marginLeft: '0.5em' }}>Edit User</Button>
+            </Menu.Item>
+          </Menu>
+        </Container>
+        <video id="bg-video" autoplay="true" loop="loop" preload="metadata" muted="muted">
+          <source src="https://10-lvl3-pdl.vimeocdn.com/01/4251/4/121257225/342565508.mp4?expires=1516812163&token=06aff2b8a111ae4b070f5" />
+        </video>
+        <Container text textAlign='center'>
+          <div>
+            <Header
+              as='h1'
+              content='Edit an Existing User'
+              inverted
+              style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
+            />
+            <Form onSubmit={this.editUser}>
+              <Form.Group widths='equal'>
+                <Form.Select fluid label='Select Player' id="selectedUser" options={this.createOptionsForForm()} placeholder='Player' selection
+                value={this.state.selectedUser} onChange={this.handleSelect}/>
+                <Form.Input fluid label='Name' placeholder='Your name' id="name" value={this.state.name} onChange={this.handleChange} />
+                <Form.Input fluid label='Bio' placeholder='Bio' id="bio" value={this.state.bio} onChange={this.handleChange}/>
+                <Form.Input fluid label='Image Link' placeholder='Image Link' id="image" value={this.state.image} onChange={this.handleChange}/>
+
+            </Form.Group>
+              <Button primary size='huge' type="submit">
+                Confirm
+                <Icon name='right arrow' />
+              </Button>
+              <Button negative size='huge' onClick={this.deleteUser}>
+                Delete
+                <Icon name='home' style={{marginLeft: "0.5em"}}/>
+              </Button>
+            </Form>
+          </div>
+        </Container>
       </div>
+
+
     )
   }
 }
 
-export default EditUser
+export default EditUser;
+
+// <div>
+//   <h1>Edit a User</h1>
+//   <form onSubmit={this.editUser}>
+//     <select value={this.state.selectedUser} onChange={this.handleSelect} id="selectedUser">
+//       {this.state.players.map(player => <option value={player.id} key={player.id}>{player.name}</option>)}
+//     </select>
+//     <input type="text" id="name" value={this.state.name} onChange={this.handleChange}/>
+//     <input type="text" id="bio" value={this.state.bio} onChange={this.handleChange}/>
+//     <input type="text" id="image" value={this.state.image} onChange={this.handleChange}/>
+//     <button type="submit" >Confirm</button>
+//   </form>
+//   <button onClick={this.deleteUser}>Delete</button>
+// </div>
--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Menu, Container, Button, Header, Icon, Dropdown, Segment, Form} from 'semantic-ui-react';
+import { BrowserRouter as Router, Route, Redirect, Link } from 'react-router-dom';
 
 
-class EditUser extends React.Component {
+
+class NewUser extends React.Component {
 
   constructor(){
     super()
@@ -38,22 +40,68 @@ class EditUser extends React.Component {
 
 
   render() {
-    return (<div>
-      <h1>Create a New User</h1>
-      <form onSubmit={this.createUser}>
-        <label>Name</label>
-        <input type="text" id="name" value={this.state.name} onChange={this.handleChange} />
-        <label>Bio</label>
-        <input type="text" id="bio" value={this.state.bio} onChange={this.handleChange}/>
-        <label>Image Link</label>
-        <input type="text" id="image" value={this.state.image} onChange={this.handleChange}/>
-        <button type="submit" >Submit</button>
-      </form>
-      <Link to="/startgame">
-        <button >Back to Start Game Page</button>
-      </Link>
-    </div>)
+    return (
+      <div >
+        <Container>
+          <Menu  inverted pointing secondary size='large'>
+            <Menu.Item as={Link} to="startgame" active>Home</Menu.Item>
+            <Menu.Item as='a'>Careers</Menu.Item>
+            <Menu.Item as='a'>Add a Question</Menu.Item>
+            <Menu.Item as='a'>Api</Menu.Item>
+            <Menu.Item position='right'>
+              <Button as={Link} to="new" inverted className={"white-link"}>Add User</Button>
+              <Button as={Link} to="edit" inverted style={{ marginLeft: '0.5em' }}>Edit User</Button>
+            </Menu.Item>
+          </Menu>
+        </Container>
+        <video id="bg-video" autoplay="true" loop="loop" preload="metadata" muted="muted">
+          <source src="https://10-lvl3-pdl.vimeocdn.com/01/4251/4/121257225/342565508.mp4?expires=1516812163&token=06aff2b8a111ae4b070f5" />
+        </video>
+        <Container text textAlign='center'>
+          <div>
+            <Header
+              as='h1'
+              content='Create a New User'
+              inverted
+              style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
+            />
+            <Form onSubmit={this.createUser}>
+              <Form.Group widths='equal'>
+                <Form.Input fluid label='Name' placeholder='Your name' id="name" value={this.state.name} onChange={this.handleChange} />
+                <Form.Input fluid label='Bio' placeholder='Bio' id="bio" value={this.state.bio} onChange={this.handleChange}/>
+                <Form.Input fluid label='Image Link' placeholder='Image Link' id="image" value={this.state.image} onChange={this.handleChange}/>
+              </Form.Group>
+              <Button primary size='huge' type="submit">
+                Submit
+                <Icon name='right arrow' />
+              </Button>
+              <Button secondary size='huge' as={Link} to="startgame">
+                Main Menu
+                <Icon name='home' style={{marginLeft: "0.5em"}}/>
+              </Button>
+            </Form>
+          </div>
+        </Container>
+      </div>
+      )
   }
 }
 
-export default EditUser
+export default NewUser;
+
+
+// <div>
+//   <h1>Create a New User</h1>
+//   <form onSubmit={this.createUser}>
+//     <label>Name</label>
+//     <input type="text" id="name" value={this.state.name} onChange={this.handleChange} />
+//     <label>Bio</label>
+//     <input type="text" id="bio" value={this.state.bio} onChange={this.handleChange}/>
+//     <label>Image Link</label>
+//     <input type="text" id="image" value={this.state.image} onChange={this.handleChange}/>
+//     <button type="submit" >Submit</button>
+//   </form>
+//   <Link to="/startgame">
+//     <button >Back to Start Game Page</button>
+//   </Link>
+// </div>

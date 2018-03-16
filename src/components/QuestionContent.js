216ee170bab3c35cb938f9dd6b4d4c9e9374b155
bmcilhenny <bmcilhen@gmail.com>
2018-03-16 13:33:03 -0400

    new branch reveal-component

--- src/components/QuestionContent.js
+++ src/components/QuestionContent.js
@@ -23,7 +23,7 @@ class QuestionContent extends React.Component {
       play: false
     }
   }
-
+// #
   shuffle = (a) => {
       for (let i = a.length - 1; i > 0; i--) {
           const j = Math.floor(Math.random() * (i + 1));
